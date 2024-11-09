e0e06d34d96c6437fdcac5a92f5eb0618122e903
AKSHAY R <113174845+AkshayKanathur@users.noreply.github.com>
2024-11-09 10:57:23 +0530

    Update install_hadoop.sh

--- install_hadoop.sh
+++ install_hadoop.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/bash
 
 # Reconfigure any unpacked but unconfigured packages
-# sudo dpkg --configure -a && \
+if ! sudo dpkg --configure -a; then
+    echo "dpkg encountered an error."
+fi
 
 # Updating system and installing Docker
-# sudo apt-get update && \
-sudo apt-get install -y docker.io || \
-sudo snap install -y docker && \
+if ! sudo apt-get update || ! sudo apt-get install -y docker.io; then
+    echo "apt failed to install Docker, attempting snap installation."
+    sudo snap install docker
+fi
 
-sudo cp start-hadoop /usr/local/bin && \
+# Copying the start-hadoop script to /usr/local/bin
+sudo cp start-hadoop /usr/local/bin
 
 # Pulling the Hadoop Docker Image
-sudo docker pull sequenceiq/hadoop-docker:2.7.1 && \
+sudo docker pull sequenceiq/hadoop-docker:2.7.1
 
 # Running the Container
 sudo docker run -it -p 50070:50070 -p 8088:8088 sequenceiq/hadoop-docker:2.7.1 /etc/bootstrap.sh -bash
